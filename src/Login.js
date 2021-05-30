447b3ded26fc87dd90ba7912aba840216caecce8
Rakshit <rakshitpanjwani@gmail.com>
2021-05-30 19:50:30 +0530

    changed redirect url

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import SpotifyLogo from "./assets/BigSpotify";
 
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=a66eca645e8847fbbb3934715b898a9a&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  "https://accounts.spotify.com/authorize?client_id=a66eca645e8847fbbb3934715b898a9a&response_type=code&redirect_uri=https://rakshit-spotify.web.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
   return ( 
